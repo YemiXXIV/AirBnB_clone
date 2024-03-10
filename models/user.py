029e633bf5b38406fc0f5bd37049b4e0eba5f61c
Mustapha <mustapha.yinusa@yahoo.com>
2024-03-10 23:05:51 +0100

    New

--- models/user.py
+++ models/user.py
@@ -10,15 +10,8 @@ class User(BaseModel):
     """
     User class that inherits from BaseModel
     """
-    def __init__(self, *args, **kwargs):
-        """Initialize User instance"""
-        super().__init__(*args, **kwargs)
-        self.email = kwargs.get('email', "")
-        self.password = kwargs.get('password', "")
-        self.first_name = kwargs.get('first_name', "")
-        self.last_name = kwargs.get('last_name', "")
 
-    def __str__(self):
-        """Return string representation of User instance"""
-        return "[{}] ({}) {}".format(
-            type(self).__name__, self.id, self.__dict__)
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
