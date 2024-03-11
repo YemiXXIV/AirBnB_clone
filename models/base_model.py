0479bdac278037a55c43e9b3ac45ab26b972c46f
Mustapha <mustapha.yinusa@yahoo.com>
2024-03-11 13:38:26 +0100

    Task 9 new

--- models/base_model.py
+++ models/base_model.py
@@ -32,7 +32,7 @@ class BaseModel:
                     self.updated_at = val
                 elif arg != "__class__":
                     setattr(self, arg, kwargs[arg])
-         else:
+        else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
