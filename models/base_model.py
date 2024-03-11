8b0a26c7e1690554e4ac49d9b8f029144ada5bc4
Mustapha <mustapha.yinusa@yahoo.com>
2024-03-11 13:31:42 +0100

    Task 9 new

--- models/base_model.py
+++ models/base_model.py
@@ -20,18 +20,24 @@ class BaseModel:
         Initializes when an instance of BaseModel is called
         """
         if kwargs:
-            for key, value in kwargs.items():
-                if key == 'created_at' or key == 'updated_at':
-                    setattr(self, key, datetime.strptime(
-                        value, '%Y-%m-%dT%H:%M:%S.%f'))
-                elif key != '__class__':
-                    setattr(self, key, value)
-        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            for arg in kwargs:
+                if arg == "created_at":
+                    val = datetime.strptime(kwargs[arg], '%Y-%m-%dT%H:%M:%S.%f')
+                    self.created_at = val
+                elif arg == "updated_at":
+                    val = datetime.strptime(kwargs[arg], '%Y-%m-%dT%H:%M:%S.%f')
+                    self.updated_at = val
+                elif arg != "__class__":
+                    setattr(self, arg, kwargs[arg])
+         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
             storage.new(self)
-
+            
     def __str__(self):
         """
         Return string representation of basemodel instance
