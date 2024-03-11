e21d1bab2da74cc83469dd4dfaa8331badfc251f
Mustapha <mustapha.yinusa@yahoo.com>
2024-03-11 14:38:56 +0100

    Changes

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -52,7 +52,7 @@ class FileStorage:
             from models.base_model import BaseModel
             from models.user import User
             from models.amenity import Amenity
-            from modles.review import Review
+            from models.review import Review
             from models.city import City
             from models.place import Place
             from models.state import State
--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -6,86 +6,48 @@ Test Case Module for User Module
 from unittest import TestCase
 from models.user import User
 from datetime import datetime
+from models import storage
+from models.base_model import BaseModel
 
 
 class TestUser(TestCase):
     """
     Test Class for Base Model
     """
-    def test_no_arg(self):
-        """Test instance creation with no arguments
-        """
-        tmp = User()
-        no_list = []
-        check_list = [
-                'id', 'created_at',
-                'updated_at', '__class__'
-                ]
-        for key in tmp.to_dict():
-            no_list.append(key)
-        self.assertEqual(no_list, check_list)
+    @classmethod
+    def setUpClass(cls):
+        """Setup the unittest"""
+        cls.user = User()
+        cls.user.email = "mustapha.yinusa@gmail.com"
+        cls.user.password = "1946"
+        cls.user.first_name = "Mustapha"
+        cls.user.last_name = "Yinusa"
 
-    def test_str(self):
-        """
-        Test instance creation with string
-        """
-        tmp_str = User()
-        string_name = type(tmp_str).__name__
-        string_id = tmp_str.id
-        string_dict = str(tmp_str.__dict__)
-        output = "[{}] ({}) {}".format(string_name, string_id, string_dict)
-        self.assertEqual(str(tmp_str), output)
+    def test_for_instantiation(self):
+        """Tests instantiation of User class."""
+        user = User()
+        self.assertEqual(str(type(user)), "<class 'models.user.User'>")
+        self.assertIsInstance(user, User)
+        self.assertTrue(issubclass(type(user), BaseModel))
 
-    def test_email_attribute(self):
-        """
-        Test class attributes
-        """
-        usr = User()
-        self.assertEqual(usr.email, "")
-        self.assertEqual(usr.password, "")
-        self.assertEqual(usr.first_name, "")
-        self.assertEqual(usr.last_name, "")
+    def test_type(self):
+        self.assertEqual(type(User()), User)
 
-    def test_kwargs_args(self):
-        """
-        Tests instance creation with kwargs arguments
-        """
-        tmp_kwgs = User(name='Mustapha', age=24, time='now')
-        no_list_kwgs = []
-        check_list_kwgs = [
-            'name', 'age', 'time', '__class__',
-                ]
-        for key in tmp_kwgs.to_dict():
-            no_list_kwgs.append(key)
-        self.assertEqual(no_list_kwgs, check_list_kwgs)
+    def test_new_instance_stored_in_objects(self):
+        self.assertIn(User(), storage.all().values())
 
-    def test_args_args(self):
-        """
-        Tests instance creation with args arguments
-        """
-        tmp_args = User(13, "new", "anything", 34.50)
-        nolist_args = []
-        check_list = [
-                'id', 'created_at',
-                'updated_at', '__class__'
-                ]
-        for key in tmp_args.to_dict():
-            nolist_args.append(key)
-        self.assertEqual(nolist_args, check_list)
+    def test_attrs(self):
+        self.assertTrue('id' in self.user.__dict__)
+        self.assertTrue('email' in self.user.__dict__)
+        self.assertTrue('first_name' in self.user.__dict__)
+        self.assertTrue('last_name' in self.user.__dict__)
+        self.assertTrue('password' in self.user.__dict__)
+        self.assertTrue('created_at' in self.user.__dict__)
+        self.assertTrue('updated_at' in self.user.__dict__)
 
-    def test_args_and_kwargs(self):
-        """
-        Tests instance creation with args and kwargs
-        """
-        tmp_args_kwargs = User("every", "new", 23, name="Mustapha", age=20)
-        list_ak = []
-        check_list = [
-            'name', 'age', '__class__',
-                ]
-        for key in tmp_args_kwargs.to_dict():
-            list_ak.append(key)
-        self.assertEqual(list_ak, check_list)
+    def test_to_dict(self):
+        self.assertTrue('to_dict' in dir(self.user))
 
         
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
