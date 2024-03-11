f923a58ccbfc67d5bcca87f46c30035e81f7fed6
Mustapha <mustapha.yinusa@yahoo.com>
2024-03-11 13:43:51 +0100

    Task 9 new

--- models/state.py
+++ models/state.py
@@ -10,5 +10,10 @@ from models.base_model import BaseModel
 class State(BaseModel):
     """
     Class for State
+    
+    Attributes:
+    name (str): The name of the state
+    
     """
+
     name = ""
