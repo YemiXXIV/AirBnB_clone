586aceecaf87f2f14f86d61ab7bd9ca4607dec75
Mustapha <mustapha.yinusa@yahoo.com>
2024-03-11 14:20:12 +0100

    Task 9 again

--- models/state.py
+++ models/state.py
@@ -1,19 +1,15 @@
 #!/usr/bin/python3
-
 """
-Module for State
+This defines the State class.
 """
-
 from models.base_model import BaseModel
 
 
 class State(BaseModel):
-    """
-    Class for State
-    
+    """This represent a state.
+
     Attributes:
-    name (str): The name of the state
-    
+        name (str): The name of the state.
     """
 
     name = ""
